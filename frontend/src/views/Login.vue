1b109162c6f51c42b75b078be0a99b344dc1e3c0
Ollie Stone <ollieandy.stone@gmail.com>
2023-02-25 14:14:47 +0000

    Art

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,8 +1,12 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <h1>Hello World</h1>
+  <main id="login-view">
+    <h1>Hello Wolrd</h1>
+    <form id="login-form">
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="password" />
+      <button>Login</button>
+    </form>
   </main>
 </template>
