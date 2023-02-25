e1f7bbc30644c5e8010c0dcf74cf574288ccfd1f
Ollie Stone <ollieandy.stone@gmail.com>
2023-02-25 15:05:54 +0000

    Add login failed button

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -3,10 +3,31 @@
 <template>
   <main id="login-view">
     <h1>Hello Wolrd</h1>
-    <form id="login-form">
+    <form id="login-form" onsubmit="return false;">
+      <p
+        class="error"
+        :style="{ visibility: err_login_failed ? 'visible' : 'hidden' }"
+      >
+        Error: invalid username or password
+      </p>
       <input type="text" placeholder="Username" />
-      <input type="password" placeholder="password" />
-      <button>Login</button>
+      <input type="password" placeholder="Password" />
+      <button @click="login">Login</button>
     </form>
   </main>
 </template>
+
+<script>
+export default {
+  methods: {
+    login() {
+      this.err_login_failed = true;
+    },
+  },
+  data() {
+    return {
+      err_login_failed: false,
+    };
+  },
+};
+</script>
