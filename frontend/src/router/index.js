d868afb425ba055f606a5db9db74595ce600f1f2
James <lim.en.hui.james@gmail.com>
2023-02-25 14:42:43 +0000

    Search view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/Login.vue";
+import Search from "../views/Search.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Login,
-    },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: Login,
+        },
+        {
+            path: "/search",
+            name: "search",
+            component: Search,
+        }
+    ],
 });
 
 export default router;
---
+++ frontend/src/views/Search.vue
@@ -0,0 +1,33 @@
+<script setup></script>
+
+<template>
+  <main>
+    <div id="header-bar">
+      <div id="user-greeting">
+        <p>Hi, {{ user }}!</p>
+      </div>
+    </div>
+    <div id="search-div">
+      <h1>What do you want to learn?</h1>
+      <div id="search-inner-div">
+        <div id="search-bar">
+          <input type="text" />
+          <div id="search-btn">
+            <button>Teach me senpai</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import "@/assets/search.scss";
+export default {
+  data() {
+    return {
+      user: "testing",
+    };
+  },
+};
+</script>
