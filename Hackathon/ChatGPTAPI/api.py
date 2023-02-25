ed59a2f2ab678760c5bbc054cbbaf3cc0edabb92
JKoenigUCL <jasper.koenig.22@ucl.ac.uk>
2023-02-25 14:25:33 +0000

    Setup ChatGPT Wrapper

--- Hackathon/ChatGPTAPI/api.py
+++ Hackathon/ChatGPTAPI/api.py
@@ -6,17 +6,25 @@ import ast
 
 app = Flask(__name__)
 api = Api(app)
+
 chatGPT = ChatGPT()
 
 # This is the class that will get a request with a topic, and return ChatGPT's response
-class ChatGPT(Resource):
+class ChatGPTAPI(Resource):
     def post(self):
         # Get the topic from the request
         parser = reqparse.RequestParser()
         parser.add_argument('topic')
         args = parser.parse_args()
         topic = args['topic']
+        # Format request 
+        request = "Create a list of subtopics required to understand " + topic
         # Get the response from ChatGPT
-        response = chatgpt.get_response(topic)
+        response = chatGPT.get_response(topic)
         # Return the response
         return response
+    
+api.add_resource(ChatGPTAPI, '/chatgpt')
+
+if __name__ == '__main__':
+    app.run()  # run our Flask app
